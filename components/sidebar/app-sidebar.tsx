b5a357140780135a222104064a63d9408a16e1a2
HwichulShin <shinhwiiron@gmail.com>
2025-03-12 17:21:14 +0900

    Modify sidebar AutoLabs logo

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -3,6 +3,11 @@
 import * as React from "react";
 import Link from "next/link";
 import data from "@/mocks/sidebarData";
+import { useTheme } from "next-themes";
+import LogoWhite from "@/public/images/icon-logo-white.svg";
+import LogoBlack from "@/public/images/icon-logo-black.svg";
+import LogoWhiteS from "@/public/images/icon-logo-s-white.svg";
+import LogoBlackS from "@/public/images/icon-logo-s-black.svg";
 import { NavMain } from "@/components/sidebar/nav-main";
 import { NavUser } from "@/components/sidebar/nav-user";
 import {
@@ -11,15 +16,32 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
+  useSidebar,
 } from "@/components/ui/sidebar";
-import Logo from "@/public/images/icon-logo-gradient.svg";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { theme } = useTheme();
+  const { state } = useSidebar();
+
+  React.useEffect(() => {
+    console.log("Sidebar 상태:", state === "collapsed" ? "접힘" : "펼침");
+  }, [state]);
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <Link href={"/"} className="relative w-[120px]">
-          <Logo className="w-full h-[40px]" />
+        <Link href="/" className="relative w-[105px]">
+          {state === "collapsed" ? (
+            theme === "light" ? (
+              <LogoBlackS className="size-[35px]" />
+            ) : (
+              <LogoWhiteS className="size-[35px]" />
+            )
+          ) : theme === "light" ? (
+            <LogoBlack className="w-full h-[35px]" />
+          ) : (
+            <LogoWhite className="w-full h-[35px]" />
+          )}
         </Link>
       </SidebarHeader>
 
